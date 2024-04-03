7a6c2fc18a2cef9d2a6a13f22ebd4c78605587c4
yantowsky <it.yantowsky@gmail.com>
2024-04-03 16:40:08 +0300

    Added Navlink on Logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,16 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
     return (
         <header className="header">
-            <a className="logo" href="/">
+            <NavLink className="logo" to="/">
                 Pizza Day
-            </a>
+            </NavLink>
             <nav className="header__nav">
-                <NavLink className="header_link" to='/login'>Login</NavLink>
-                <NavLink className="header_link" to='/menu'>Menu</NavLink>
+                <NavLink className="header_link" to='/login'>
+                    Login
+                </NavLink>
+                <NavLink className="header_link" to='/menu'>
+                    Menu
+                </NavLink>
             </nav>
         </header>
     );
