3f52e7d7b5c067b18be2197ea30f90d208f1ee35
yantowsky <it.yantowsky@gmail.com>
2024-04-20 12:55:26 +0300

    .\.eslintrc.cjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
 import './App.css';
+import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
+
 import Header from './components/Header/Header.jsx';
-import UserNewOrder from './pages/UserNewOrder.jsx';
-import Main from './pages/Main.jsx';
-import Login from './pages/Login.jsx';
-import Menu from './pages/Menu/Menu.jsx';
-import Fetch from './pages/Fetch.jsx';
-import Counter from './pages/Counter.jsx';
+
+const UserNewOrderLazy = lazy(() => import('./pages/UserNewOrder.jsx'));
+const MainLazy = lazy(() => import('./pages/Main.jsx'));
+const LoginLazy = lazy(() => import('./pages/Login.jsx'));
+const MenuLazy = lazy(() => import('./pages/Menu/Menu.jsx'));
+const FetchLazy = lazy(() => import('./pages/Fetch.jsx'));
+const CounterLazy = lazy(() => import('./pages/Counter.jsx'));
 
 
 function App() {
     return (
         <div className="wrapper">
             <Header />
-            <Routes>
-                <Route path='/' element={<Main />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/menu' element={<Menu />} />
-                <Route path='/order/new' element={<UserNewOrder />} />
-                <Route path='/fetch' element={<Fetch />} />
-                <Route path='/counter' element={<Counter/>} />
-            </Routes>
+            <Suspense fallback={
+                <main className="content">
+                    <h1 className="title">
+                        Loading...
+                    </h1>
+                </main>
+            }>
+                <Routes>
+                    <Route path='/' element={<MainLazy />} />
+                    <Route path='/login' element={<LoginLazy />} />
+                    <Route path='/menu' element={<MenuLazy />} />
+                    <Route path='/order/new' element={<UserNewOrderLazy />} />
+                    <Route path='/fetch' element={<FetchLazy />} />
+                    <Route path='/counter' element={<CounterLazy />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 }
